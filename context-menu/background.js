7466c3c8ea49404a0e5a2f4f8c032454e925803c
gaoqing <shaozhan2004@126.com>
2023-12-09 12:32:43 +0800

    enhance context menu to do translate and popup notification and click it will open next detail tab

--- context-menu/background.js
+++ context-menu/background.js
@@ -1,18 +1,98 @@
-
 chrome.runtime.onInstalled.addListener(async () => {
     chrome.contextMenus.create({
         id: "ICIBA",
-        title:  "iciba",
+        title: "iciba",
         type: 'normal',
         contexts: ['selection']
     });
 });
 
+
+const lastQuery = {};
+
 chrome.contextMenus.onClicked.addListener((item, tab) => {
-    const url = new URL(`https://www.iciba.com/word`);
-    url.searchParams.set('w', item.selectionText);
-    chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+    const word = item.selectionText.toLowerCase();
+    fetch(`https://dict.iciba.com/dictionary/word/suggestion?word=${word}`,
+        {method: 'GET', headers: {"User-Agent": "curl/8.1.2", Host: "dict.iciba.com"}})
+        .then(m => m.json())
+        .then(m => m.message.filter(x => x.key.toLowerCase() === word))
+        .then(m => m[0].paraphrase)
+        .then(m => {
+            if (!m || m.trim().length === 0) {
+                throw new Error("force into catch block to search in a new tab");
+            }
+
+            const notificationId = `${Date.now()}`;
+            lastQuery.id = notificationId;
+            lastQuery.word = word;
+            lastQuery.tab = tab;
+            chrome.notifications.create(notificationId, {
+                message: `${m}`,
+                type: "basic", iconUrl: '16px.png', title: word, eventTime: Date.now() + 30 * 1000
+            })
+        })
+        .catch(e => {
+            const url = new URL(`https://www.iciba.com/word`);
+            url.searchParams.set('w', word);
+            chrome.tabs.create({url: url.href, index: tab.index + 1});
+        })
 });
+
+chrome.notifications.onClicked.addListener(
+    (notificationId) => {
+        const {id, tab, word} = lastQuery;
+        if (id === `${notificationId}`) {
+            const url = new URL(`https://www.iciba.com/word`);
+            url.searchParams.set('w', word);
+            chrome.tabs.create({url: url.href, index: tab.index + 1});
+        }
+    }
+)
+
+chrome.notifications.onClosed.addListener(
+    notificationId => chrome.notifications.clear(notificationId)
+)
+
+
+const activeTabMemory = {};
+
+chrome.tabs.onActivated.addListener(
+    activeInfo => {
+        const {tabId, windowId} = activeInfo;
+        let tabActivationList = activeTabMemory[windowId];
+        if (!tabActivationList) {
+            activeTabMemory[windowId] = [];
+            tabActivationList = activeTabMemory[windowId]
+        }
+        const findId = tabActivationList.findIndex(id => id === tabId);
+        if (findId !== -1) {
+            tabActivationList.splice(findId, 1);
+        }
+
+        tabActivationList.push(tabId);
+    }
+)
+
+chrome.tabs.onRemoved.addListener(
+    (tabId, {windowId, isWindowClosing}) => {
+        if (isWindowClosing) {
+            delete activeTabMemory[windowId];
+        } else {
+            const tabList = activeTabMemory[windowId];
+            const currentTab = tabList[tabList.length - 1];
+            if (tabId === currentTab) {
+                tabList.pop();
+                const nextFocusTabId = tabList.pop();
+                chrome.tabs.update(nextFocusTabId, {active: true})
+            } else {
+                const closeId = tabList.findIndex(id => id === tabId);
+                if (closeId !== -1) {
+                    tabList.splice(closeId, 1);
+                }
+            }
+        }
+    }
+)
 
 /*
 * do multiple plugin when want to only create top level to save navigating to child list
